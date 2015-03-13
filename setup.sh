7492e1be52510fd249cff62acdd6adc89a6937f2
Pierre-Yves Mathieu <pierre-yves@pywebdesign.net>
2015-03-13 10:45:40 -0400

    add sudo, many needs it for docker

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-docker-compose build
-docker-compose run db ls
-docker-compose run web ls
-docker-compose run web bundle install
-docker-compose run web rails new . --database=postgresql
+sudo docker-compose build
+sudo docker-compose run db ls
+sudo docker-compose run web ls
+sudo docker-compose run web bundle install
+sudo docker-compose run web rails new . --database=postgresql
 . unlock.sh
 cp -f database.setup.yml config/database.yml
-docker-compose run db ls
-docker-compose run web rake db:create
+sudo docker-compose run db ls
+sudo docker-compose run web rake db:create
